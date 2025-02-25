22bf1af41ab5f5a2e61acd74143d02cbad76b761
Zeyi Zhang <zcapzz4@ucl.ac.uk>
2025-02-25 16:11:42 +0000

    added 50 GeV mass cut before training

--- weight_ONLY_TRAINS.py
+++ weight_ONLY_TRAINS.py
@@ -42,6 +42,10 @@ def main():
         if hasattr(graph, 'pt'):
             delattr(graph, 'pt')
 
+    for jet in dataset:
+        if jet.mass <= 50:
+            dataset.remove(jet)
+
     # check the number of signal and background jets
     labels = [data.y for data in dataset]
     num_signal = labels.count(1)
